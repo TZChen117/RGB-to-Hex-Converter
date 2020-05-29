4cc512ba06ab8d2859040755bb3d1f36aa7b73fe
TZChen <x5713191@hotmail.com>
2020-05-29 10:38:14 +0800

    add checking value integer and non-number in main.js

--- main.js
+++ main.js
@@ -11,19 +11,23 @@ const btn = document.querySelector('.btn')
 // 網址:http://www.thecolorapi.com/id?format=svg&named=false&hex=0047AB
 const url = 'https://www.thecolorapi.com/id?format=svg&named=false&'
 
+// 輸入資料確認是否為數字
+
+
 // 在按鈕上安裝事件監聽器
 btn.addEventListener('click', function () {
   if (rIndex.value === '' || gIndex.value === '' || bIndex.value === '') {
-    window.alert('Please keyin content!!!')
-  } else if (rIndex.value > 255 || rIndex.value < 0 || gIndex.value > 255 || gIndex.value < 0 || bIndex.value > 255 || bIndex.value < 0) {
+    window.alert('Please key-in content!!!')
+  } else if (rIndex.value > 255 || rIndex.value < 0 || (rIndex.value % 1) !== 0 || gIndex.value > 255 || gIndex.value < 0 || (gIndex.value % 1) !== 0 || bIndex.value > 255 || bIndex.value < 0 || (bIndex.value % 1) !== 0) {
     window.alert('Incorrect index!!!')
-  } else {
+  } else if (isNaN(rIndex.value) || isNaN(gIndex.value) || isNaN(bIndex.value)){
+    window.alert('Key-in not number!!!')
+  } else{
     convertHex()
   }
 })
 
 // 取得RGB index數值，#00(R)00(G)00(B)
-
 function convertHex(index) {
   let redHex = Number(rIndex.value).toString(16)
   let greenHex = Number(gIndex.value).toString(16)
